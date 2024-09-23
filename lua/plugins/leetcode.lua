98c0dcdba6d3490d9b5e378e47db13ec47dc537c
l4m4r <dev@vophan.day>
2024-09-23 12:02:04 +0800

    add leetcode plugin

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,21 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+
+    -- optional
+    "nvim-treesitter/nvim-treesitter",
+    "rcarriga/nvim-notify",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    cn = { -- leetcode.cn
+      enabled = true, ---@type boolean
+      translator = true, ---@type boolean
+      translate_problems = true, ---@type boolean
+    },
+  },
+}
